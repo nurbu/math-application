ddb4614b7984cdd2d9eed0ab4184b79b475a5907
Nurbu <lamanurbu2@gmail.com>
2026-04-08 10:12:59 -0400

    Question 2 solved

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+        // Finding the highest salary between Bob and Gary
+
+        double bobSalary = 40200;
+        double garySalary = 12000.12;
+        double highestSalary = Math.max(bobSalary, garySalary);
+
+        System.out.println("The Highest salary is: $" + highestSalary);
+
+        // Finding the lower price between a car and truck.
+
+        double carPrice = 3002.0;
+        double truckPrice = 3012.12;
+        double lowestPrice = Math.min(truckPrice, carPrice);
+        System.out.println("Lowest price vehicle is: $" + lowestPrice);
+
+        //
+    }
 }
